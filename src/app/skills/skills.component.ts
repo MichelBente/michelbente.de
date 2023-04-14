eb58cd95630a2b60300ef555850caceda4ce34af
Michel Bente <bentemichel@web.de>
2023-04-14 15:27:44 +0200

    skillsection

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./skills.component.scss']
 })
 export class SkillsComponent {
-
+  skills = ['Angular', 'TypeScript', 'JavaScript','CSS/SCSS', 'HTML', 'GIT', 'Firebase', 'Scrum', 'Rest-API', 'Material Design']
+  skillsImg = ['angular.png', 'typescript.png', 'javascript.png', 'css.png', 'html.png', 'git.png', 'firebase.png', 'scrum.png', 'api.png', 'material.png']
 }
