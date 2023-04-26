a184f8b94cd1be3b8331452d18806cb8d427137c
Michel Bente <bentemichel@web.de>
2023-04-26 17:32:38 +0200

    contactarea

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,39 +1,70 @@
-import { Component, OnInit, ViewChild, ElementRef, } from '@angular/core';
+import { Component, OnInit, } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.scss']
 })
-export class ContactComponent implements OnInit {
-  // @ViewChild('myForm') myForm!: ElementRef;
-  // @ViewChild('nameField') myForm!: ElementRef;
-  // @ViewChild('messageField') myForm!: ElementRef;
-  // @ViewChild('sendButton') myForm!: ElementRef;
+export class ContactComponent {
+  isSubmitted = false;
+  emailSent: boolean = false;
+
+  myForm = new FormGroup({
+    name: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    message: new FormControl('', [Validators.required, Validators.minLength(10)])
+  })
+
 mailmestring = "mailto:bentemichel@web.de";
 
-constructor() {}
+constructor(private http: HttpClient) {}
+post = {
+  endPoint: 'https://michel-helmut-bente.developerakademie.net/michelbente/src/send_mail.php',
+  body: (payload: any) => JSON.stringify(payload),
+  options: {
+    headers: {
+      'Content-Type': 'text/plain',
+      responseType: 'text',
+    },
+  },
+};
+onSubmit(ngForm: any) {
+  this.isSubmitted = true;
+  if (this.myForm.valid){
+    this.isSubmitted = false;
+    this.myForm.reset();
+  } 
+  if (this.myForm.valid) {
+    let data = {
+      name: this.myForm.value.name,
+      email: this.myForm.value.email,
+      message: this.myForm.value.message
+    }
+    this.http
+      .post(this.post.endPoint, data)
+      .subscribe({
+        next: (response) => {
+          ngForm.resetForm()
+          this.emailSent = true;
+          this.timeOutSendMail();
+        },
+        error: (error) => {
+        },
+      });
+  } else {
+  }
+}
+timeOutSendMail() {
+  setTimeout(() => {
+    this.emailSent = false;
+  }, 2000)
 
-ngOnInit(): void {
+}
 }
 
-sendMail() {
- // action="https://michel-helmut-bente.developerakademie.net/send_mail/send_mail.php"
-// console.log('Sending Mail', this.myForm);
-// let nameField = this.nameField.nativeElement.disabled = true;
-// let messageField = this.nameField.nativeElement.disabled = true;
-// let sendButton = this.nameField.nativeElement.disabled = true;
-// nameField.disabled = true;
-// messageField.disabled = true;
-// sendButton.disabled = true;
-
-// //senden
-// nameField.disabled = true;
-// messageField.disabled = true;
-// sendButton.disabled = true;
 
 
-}
 
-
-}
